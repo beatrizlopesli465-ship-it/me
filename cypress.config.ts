569b51b5d0f31a99c8f167df28592da0be94d671
beatrizlopesli465-ship-it <beatriz.lopesli465@gmail.com>
2025-11-22 16:53:29 -0300

    Add files via upload

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:6006", 
+    // setupNodeEvents(on, config) {
+    // },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
